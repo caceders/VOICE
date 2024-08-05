07805fc9a4159c480c8fc97c68288436f4b45629
Carl Anders J. Cederström <andersjohnsgaard@hotmail.com>
2024-08-05 04:00:07 +0200

    Adjusted test to facilitate for CI pipeline

--- tests/test_real_time_voice_transcriber.py
+++ tests/test_real_time_voice_transcriber.py
@@ -4,29 +4,35 @@ import threading
 import azure.cognitiveservices.speech as speechsdk
 from src.VOICE.hearing.real_time_voice_transcriber import real_time_voice_transcriber
 
-# What does the module need to do?
-# - Set up Azure credentials
-# - Start a thread for transcribing voice
-# - Transcribe voice
-# - Send out voice transcription message
+## Mock AudioInputStream or similar components that simulate microphone input - needed for GIT CI
+class MockAudioInputStream:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def read(self):
+        # Simulate reading audio data
+        pass
 
 def test_real_time_voice_transcriber():
     
     transcription_queue = Queue()
     quit_queue = Queue()
+
+    with mock.patch('src.VOICE.hearing.real_time_voice_transcriber.speechsdk.AudioConfig', new=MockAudioInputStream):
+        transcriber = real_time_voice_transcriber(transcription_queue, quit_queue)
     
-    transcriber = real_time_voice_transcriber(transcription_queue, quit_queue)
+        mock_event = mock.Mock()
+        mock_event.result.reason = speechsdk.ResultReason.RecognizedSpeech
+        mock_event.result.text = "Test"
+        
+        transcriber_thread = threading.Thread(target= transcriber.transcribe_voice)
+        transcriber_thread.start()
 
-    mock_event = mock.Mock()
-    mock_event.result.reason = speechsdk.ResultReason.RecognizedSpeech
-    mock_event.result.text = "Test"
-    
-    transcriber_thread = threading.Thread(target= transcriber.transcribe_voice)
-    transcriber_thread.start()
+        transcriber.speech_recognizer.recognized.signal(mock_event)
 
-    transcriber.speech_recognizer.recognized.signal(mock_event)
+        result = transcription_queue.get()
+        quit_queue.put("quit")
 
-    result = transcription_queue.get()
-    quit_queue.put("quit")
+        assert (result == "Test")
 
-    assert (result == "Test")
+        transcriber_thread.join()
