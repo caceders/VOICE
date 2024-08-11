fc891fee8648409915c114c0019645fcd24fca11
Carl Anders J. Cederström <andersjohnsgaard@hotmail.com>
2024-08-12 01:51:28 +0200

    cleanup transcriber test

--- tests/test_real_time_voice_transcriber.py
+++ tests/test_real_time_voice_transcriber.py
@@ -20,7 +20,6 @@ def test_real_time_voice_transcriber():
 
     result = transcription_queue.get()
     
-    time.sleep(1) # Wait for transcriber to be finished
     quit_queue.put("quit")
     transcriber_thread.join()
 
