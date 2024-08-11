1de393567613b0f3d9deb7723efc6b016614eb84
Carl Anders J. Cederström <andersjohnsgaard@hotmail.com>
2024-08-11 18:36:29 +0200

    finalized tests for voice activity gatekeeper

--- tests/test_voice_activity_gatekeeper.py
+++ tests/test_voice_activity_gatekeeper.py
@@ -1,11 +1,3 @@
-# Test get transcribed voice through messagepassing
-# Test pass transcribed voice when not responding and not hybernating
-# Test no deliverance when respondong
-# Test deliverance when done responding.
-# Test no deliverance when hybernation
-# Test deliverance when awoken - with wakeup flag
-# Test hybernation activation [mock time]
-
 from unittest import mock
 from queue import Queue
 import threading
