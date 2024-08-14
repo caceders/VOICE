d9fc65dbd8006d2b55b9d18ba915808eaae914ec
Carl Anders J. Cederström <andersjohnsgaard@hotmail.com>
2024-08-14 16:29:57 +0200

    Removed messagepassing - not needed

--- tests/thinking/test_transcription_formater.py
+++ tests/thinking/test_transcription_formater.py
@@ -1,4 +1,3 @@
-from queue import Queue
 from src.VOICE.thinking.transcription_formater import TranscriptionFormater
 from datetime import datetime
 import pytest
@@ -6,29 +5,24 @@ from freezegun import freeze_time
 
 @pytest.mark.skip(reason="Functionality not implemented yet")
 def test_timestamp():
-    unformated_transcripts = Queue()
-    formated_transcripts = Queue()
 
-    TranscriptionFormater(unformated_transcripts, formated_transcripts)
+    TranscriptionFormater()
 
     freeze_time()
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
-    unformated_transcripts.put(("test", ""))
-    result = formated_transcripts.get()
+    formated_transcript = TranscriptionFormater.format(("test", ""))
 
-    assert result == f"[{current_time}]test"
+    assert formated_transcript == f"[{current_time}]test"
 
 
 @pytest.mark.skip(reason="Functionality not implemented yet")
 def test_wakeupstamp():
-    unformated_transcripts = Queue()
-    formated_transcripts = Queue()
+    TranscriptionFormater()
 
-    TranscriptionFormater(unformated_transcripts, formated_transcripts)
+    TranscriptionFormater()
 
     freeze_time()
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
-    unformated_transcripts.put("test", "Awoken")
-    result = formated_transcripts.get()
-    
-    assert result == f"[{current_time}][Awoken]test"
+    formated_transcript = TranscriptionFormater.format(("test", "Awoken"))
+
+    assert formated_transcript == f"[{current_time}][Awoken]test"
